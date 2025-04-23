ddb12503d0acfef6b0b2aec1ba0ab07753c1e852
NiclasNorin <niclasn@outlook.com>
2025-04-23 10:53:51 +0200

    chore: remove @dump

--- source/php/Module/views/fields/googleMap.blade.php
+++ source/php/Module/views/fields/googleMap.blade.php
@@ -6,20 +6,17 @@
         'style' => 'height: ' . $field['height'] . 'px; position: relative;',
     ]
 ])
-@element([
-    'id' => $field['name'],
-    'classList' => ['mod-frontend-form__openstreetmap'],
-    'attributeList' => [
-        'data-js-openstreetmap' => 'true',
-        'data-js-zoom' => $field['zoom'],
-        'data-js-lat' => $field['lat'],
-        'data-js-lng' => $field['lng'],
-        'style' => 'height: ' . $field['height'] . 'px; position: relative;',
-    ]
-])
-    <!-- osm -->
+    @element([
+        'id' => $field['name'],
+        'classList' => ['mod-frontend-form__openstreetmap'],
+        'attributeList' => [
+            'data-js-openstreetmap' => 'true',
+            'data-js-zoom' => $field['zoom'],
+            'data-js-lat' => $field['lat'],
+            'data-js-lng' => $field['lng'],
+            'style' => 'height: ' . $field['height'] . 'px; position: relative;',
+        ]
+    ])
+        <!-- osm -->
+    @endelement
 @endelement
-@endelement
-
-
-@dump($field)

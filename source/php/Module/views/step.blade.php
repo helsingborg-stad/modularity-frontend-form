79bdac052de74c1512d85dd9647e0b8a9a3d3518
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-04-23 15:26:46 +0200

    fix: add paper to step.

--- source/php/Module/views/step.blade.php
+++ source/php/Module/views/step.blade.php
@@ -1,22 +1,24 @@
-@if (!empty($step['fields']))
-    @element([
-        'attributeList' => [
-            'data-js-frontend-form-step' => $index
-        ],
-        'classList' => [
-            'mod-frontend-form__step',
-            $index === 0 ? 'u-display--block' : '',
-        ]
-    ])
-        @if($step['title'])
-            @typography([
-                'element' => 'h2',
-            ])
-                {{ $step['title'] }}
-            @endtypography
-        @endif
-        @foreach($step['fields'] as $field)
-            @includeIf('fields.' . $field['view'], ['field' => $field])
-        @endforeach
-    @endelement
-@endif
+@paper(['padding' => 4])
+    @if (!empty($step['fields']))
+        @element([
+            'attributeList' => [
+                'data-js-frontend-form-step' => $index
+            ],
+            'classList' => [
+                'mod-frontend-form__step',
+                $index === 0 ? 'u-display--block' : '',
+            ]
+        ])
+            @if($step['title'])
+                @typography([
+                    'element' => 'h2',
+                ])
+                    {{ $step['title'] }}
+                @endtypography
+            @endif
+            @foreach($step['fields'] as $field)
+                @includeIf('fields.' . $field['view'], ['field' => $field])
+            @endforeach
+        @endelement
+    @endif
+@endpaper

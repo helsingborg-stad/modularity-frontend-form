719b32e33af4bebf91b78ba46cf4a7ce28fa0ef7
NiclasNorin <niclasn@outlook.com>
2025-04-24 12:44:39 +0200

    feat: adding goTo for steps

--- source/js/steps/stepNavigator.ts
+++ source/js/steps/stepNavigator.ts
@@ -2,6 +2,15 @@ class StepNavigator implements StepNavigatorInterface {
     constructor(private steps: StepsObject, private activeStep: number = 0) {
     }
 
+    public goTo(id: number): StepInterface|null {
+        if (this.steps[id] && id !== this.activeStep) {
+            this.activeStep = id;
+            return this.steps[this.activeStep];
+        }
+
+        return null;
+    }
+
     public goNext(): StepInterface|null {
         if (this.activeStep < Object.keys(this.steps).length - 1) {
             this.activeStep++;

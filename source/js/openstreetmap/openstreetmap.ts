6d76201240edc7bd102688d3f9d0c448b745f5ee
NiclasNorin <niclasn@outlook.com>
2025-05-05 15:32:00 +0200

    fix: allow user to create map marker by clicking the map

--- source/js/openstreetmap/openstreetmap.ts
+++ source/js/openstreetmap/openstreetmap.ts
@@ -1,9 +1,10 @@
-import { MarkerInterface, CreateMarker, CreateMap, CreateTileLayer, TilesHelper, CreateAttribution, CreateMarkerInterface, Marker, MapInterface, CreateSearch, PlaceObject, SearchInterface } from '@helsingborg-stad/openstreetmap';
+import { MarkerInterface, CreateMarker, CreateMap, CreateTileLayer, TilesHelper, CreateAttribution, CreateMarkerInterface, MapInterface, CreateSearch, PlaceObject, SearchInterface, EventData, LatLngObject } from '@helsingborg-stad/openstreetmap';
 
 class Openstreetmap {
     private search!: SearchInterface;
     private marker: MarkerInterface|null = null;
     private map!: MapInterface;
+    private createMarker!: CreateMarkerInterface;
     constructor(
         private modularityFrontendFormData: ModularityFrontendFormData,
         private modularityFrontendFormLang: ModularityFrontendFormLang,
@@ -24,7 +25,7 @@ class Openstreetmap {
             zoom: this.zoom,
         }).create();
     
-        const createMarker = new CreateMarker();
+        this.createMarker = new CreateMarker();
 
         const tiles = new TilesHelper().getDefaultTiles('default');
         new CreateAttribution()
@@ -43,24 +44,45 @@ class Openstreetmap {
             .setApiUrl(this.modularityFrontendFormData.placeSearchApiUrl)
             .setSearchParam('q')
             .addTo(this.map)
-            .addListItemListener((e) => this.handleClick(e, createMarker));
+            .addListItemListener((e) => this.handleListItemClick(e));
+
+        this.map.addListener('click', (e) => this.handleClick(e));
     }
 
-    private handleClick(item: PlaceObject, createMarker: CreateMarkerInterface): void {
+    private handleClick(e: EventData): void {
+        if (!e.latLng) {
+            console.error('No latLng found in event data');
+            return;
+        }
+
+        if (!e.originalEvent) {
+            console.error('No originalEvent found in event data');
+            return;
+        }
+
+        if (e.originalEvent.target && e.originalEvent.target.classList.contains('mod-frontend-form__openstreetmap')) {
+            this.addOrMoveMarker(e.latLng);
+        }
+    }
+
+    private handleListItemClick(item: PlaceObject): void {
         const latLng = {lat: (item.latitude as number) ?? 0, lng: (item.longitude as number) ?? 0};
 
+        this.addOrMoveMarker(latLng);
+        this.map.flyTo(latLng, 15);
+    }
+
+    private addOrMoveMarker(latLng: LatLngObject): void {
         if (this.marker) {
             this.marker.setPosition(latLng);
         } else {
-            this.marker = createMarker.create({
+            this.marker = this.createMarker.create({
                 html: this.getIconHtml(),
                 position: latLng,
             });
 
             this.marker.addTo(this.map);
         }
-
-        this.map.flyTo(latLng, 15);
     }
 
     private getIconHtml(): string {

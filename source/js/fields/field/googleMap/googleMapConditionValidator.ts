b85c007b9681990be9e5c570610c7d723b482f88
NiclasNorin <niclasn@outlook.com>
2025-05-06 07:46:23 +0200

    feat: google maps field init

---
+++ source/js/fields/field/googleMap/googleMapConditionValidator.ts
@@ -0,0 +1,31 @@
+class GoogleMapConditionValidator implements ConditionValidatorInterface {
+    private parent: GoogleMapInterface|null = null;
+
+    public init(parent: GoogleMapInterface): void {
+        this.parent = parent;
+    }
+
+    public validate(condition: any): boolean {
+        const value = this.parent?.getOpenstreetmap().hasMarker() ? true : false;
+
+        switch (condition.operator) {
+            case '==':
+            case '=':
+            case '===':
+            case '!=empty':
+            case '==contains':
+                return value === true;
+            case '!=':
+            case '!==':
+            case '==empty':
+            case '!=contains':
+                return value === false;
+            default:
+                console.error('Invalid operator:', condition.operator);
+                return false;
+        }
+
+    }
+}
+
+export default GoogleMapConditionValidator;
---
+++ source/js/fields/field/googleMap/googleMapConditionsHandler.ts
@@ -0,0 +1,66 @@
+class GoogleMapConditionsHandler implements ConditionsHandlerInterface {
+	private fieldsObject: FieldsObject = {};
+	private parent: FieldInterface|null = null;
+	private conditions: ConditionInterface[] = [];
+	private isDisabled: boolean = false;
+
+	constructor(private unstructuredConditions: any) {
+	}
+
+	public init(parent: FieldInterface, conditionsBuilder: ConditionBuilderInterface): void {
+		this.parent = parent;
+		this.conditions = conditionsBuilder.build(this.unstructuredConditions);
+		this.setValueChangeListener();
+	}
+
+	private updateDisabled(disabled: boolean): void {
+		if (this.parent && this.isDisabled !== disabled) {
+			this.isDisabled = disabled;
+
+            this.parent.getField().classList.toggle('u-display--none', disabled);
+
+			this.dispatchUpdateEvent();
+		}
+	}
+
+	public validate(): void {
+		let isValid: boolean = false;
+
+		for (const condition of this.getConditions()) {
+			if (condition.validate()) {
+				isValid = true;
+				break;
+			}
+		}
+
+		this.updateDisabled(!isValid);
+	}
+
+	public dispatchUpdateEvent(): void {
+		if (this.parent?.getField()) {
+			this.parent.getField().dispatchEvent(new Event('modularityFrontendFormOpenstreetmapMarkerAdded'));
+		}
+	}
+
+	public getIsDisabled(): boolean {
+		return this.isDisabled;
+	}
+
+	public getConditions(): ConditionInterface[] {
+		return this.conditions;
+	}
+
+    public addValueChangeListener(field: FieldInterface): void {
+		this.fieldsObject[field.getName()] = field;
+    }
+
+	private setValueChangeListener(): void {
+        this.parent?.getField().addEventListener('modularityFrontendFormOpenstreetmapMarkerAdded', () => {
+            for (const fieldName in this.fieldsObject) {
+                this.fieldsObject[fieldName].getConditionsHandler().validate();
+            }
+        });
+	}
+}
+
+export default GoogleMapConditionsHandler;

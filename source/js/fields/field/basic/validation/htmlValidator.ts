13044eb71272a8745a68ce8da395aa8ff141af45
NiclasNorin <niclasn@outlook.com>
2025-05-14 14:11:54 +0200

    refactor: creating html validator file

---
+++ source/js/fields/field/basic/validation/htmlValidator.ts
@@ -0,0 +1,20 @@
+class HtmlValidator implements ValidationControlInterface {
+    private basic!: BasicInterface;
+
+    public init(basic: BasicInterface) {
+        this.basic = basic;
+    }
+
+    public isInvalid(): false|ValidationControlInterface {
+        const valid = this.basic.getInput().checkValidity();
+
+        return valid ? false : this;
+    }
+
+    // TODO: Translate
+    public getFailedValidationMessage(): string {
+        return 'HTML validation failed';
+    }
+}
+
+export default HtmlValidator;

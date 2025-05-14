ed232ef6d890eb5cf4ae4c25b70b87c42f356cb3
NiclasNorin <niclasn@outlook.com>
2025-05-14 14:11:39 +0200

    refactor: adding validator it to basic field

--- source/js/fields/field/basic/basic.ts
+++ source/js/fields/field/basic/basic.ts
@@ -4,13 +4,15 @@ class Basic implements BasicInterface {
         private input: HTMLInputElement,
         private name: string,
         private conditionValidator: ConditionValidatorInterface,
-        private conditionsHandler: ConditionsHandlerInterface
+        private conditionsHandler: ConditionsHandlerInterface,
+        private validator: FieldValidatorInterface
     ) {
     }
 
     public init(conditionBuilder: ConditionBuilderInterface): void {
         this.conditionsHandler.init(this, conditionBuilder);
         this.conditionValidator.init(this);
+        this.validator.init(this);
         this.listenForChanges();
     }
 
@@ -24,6 +26,10 @@ class Basic implements BasicInterface {
 
     public getConditionValidator(): ConditionValidatorInterface {
         return this.conditionValidator;
+    }
+
+    public getValidator(): FieldValidatorInterface {
+        return this.validator;
     }
 
     public getField(): HTMLElement {

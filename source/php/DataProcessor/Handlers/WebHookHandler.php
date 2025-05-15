4eb89b194fcbe71c12ce69f0f5cc13b9bd87c8f3
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-05-15 14:06:53 +0200

    fix: functioning webhook handler.

--- source/php/DataProcessor/Handlers/WebHookHandler.php
+++ source/php/DataProcessor/Handlers/WebHookHandler.php
@@ -25,17 +25,87 @@ class WebHookHandler implements HandlerInterface {
   }
 
   /**
-   * Null handler that does nothing and returns an error.
-   * This is used when no handler is found for the given data.
+   * Handle the data
+   *
+   * @param array $data The data to handle
+   * @return HandlerResultInterface|null The result of the handling
    */
   public function handle(array $data): ?HandlerResultInterface
   {
-    $this->handlerResult->setError(
+    $config = $this->moduleConfigInstance->getWebHookHandlerConfig();
+
+    if($this->validateCallbackUrl($config->callbackUrl) === false) {
+      return $this->handlerResult;
+    }
+
+    if($this->trySendRequest($config->callbackUrl, $data)) {
+      return $this->handlerResult;
+    }
+
+    return $this->handlerResult;
+  }
+
+  /**
+   * Validate the callback URL
+   *
+   * @param string $url The URL to validate
+   * @return bool True if the URL is valid, false otherwise
+   */
+  private function validateCallbackUrl(string $url): bool
+  {
+    // Validate the URL format
+    if (!filter_var($url, FILTER_VALIDATE_URL)) {
+      $this->handlerResult->setError(
+        new WP_Error(
+          "handler_error", 
+          $this->wpService->__('Invalid callback url format.', 'modularity-frontend-form')
+        )
+      );
+      return false;
+    }
+
+    // Check if the URL is reachable
+    $headers = @get_headers($url);
+    if ($headers === false) {
+      $this->handlerResult->setError(
       new WP_Error(
         "handler_error", 
-        $this->wpService->__('The webhook handler is not done yet. Please check in again in next version.', 'modularity-frontend-form')
+        $this->wpService->__('Callback url is not reachable.', 'modularity-frontend-form')
       )
-    );
-    return $this->handlerResult;
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Send the request to the webhook URL
+   *
+   * @param string $url The URL to send the request to
+   * @param array $data The data to send in the request
+   * @return bool True if the request was sent successfully, false otherwise
+   */
+  private function trySendRequest(string $url, array $data): bool
+  {
+    $response = $this->wpService->wpRemotePost($url, [
+      'body' => $data,
+      'timeout' => 20,
+      'headers' => [
+        'Content-Type' => 'application/json',
+      ],
+    ]);
+
+    if($this->wpService->isWpError($response)) {
+      $this->handlerResult->setError(
+        new WP_Error(
+          "handler_error", 
+          $this->wpService->__('Failed to send data to webhook.', 'modularity-frontend-form')
+        )
+      );
+      return false;
+    }
+
+    return true;
   }
 }

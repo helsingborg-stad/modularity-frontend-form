92c1cbc90df2ab405ad8eb5e8ee7306995073488
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-05-20 14:43:51 +0200

    fix: return wp errors in validate params to block access.

--- source/php/Api/RestApiParams.php
+++ source/php/Api/RestApiParams.php
@@ -7,6 +7,7 @@ use WpService\WpService;
 use ModularityFrontendForm\Api\RestApiParamEnums;
 use ModularityFrontendForm\Config\ModuleConfigFactory;
 use ModularityFrontendForm\Config\ConfigInterface;
+use WP_Error;
 
 class RestApiParams 
 {
@@ -52,8 +53,19 @@ class RestApiParams
       'type'        => 'integer',
       'format'      => 'uri',
       'required'    => true,
+      'sanitize_callback' => function ($moduleId) {
+          return intval($moduleId);
+      },
       'validate_callback' => function ($postId) {
-          return $this->wpService->getPost($postId) !== null;
+          $result =  $this->wpService->getPost($postId) !== null;
+          if($result === false) {
+            return new WP_Error(
+              'invalid_post_id',
+              __('The resource you are trying to find, does not exist.', 'modularity-frontend-form'),
+              ['status' => 404]
+            );
+          }
+          return true;
       }
     ];
   }
@@ -71,10 +83,22 @@ class RestApiParams
       'type'        => 'integer',
       'format'      => 'uri',
       'required'    => true,
+      'sanitize_callback' => function ($moduleId) {
+          return intval($moduleId);
+      },
       'validate_callback' => function ($moduleId) {
-          return $this->getModuleConfigInstance(
+          $result =  $this->getModuleConfigInstance(
               $moduleId
           )->getModuleIsSubmittableByCurrentUser();
+
+          if($result === false) {
+            return new WP_Error(
+              'invalid_module_id',
+              __('The forms created by this module cannot be edited at this moment.', 'modularity-frontend-form'),
+              ['status' => 404]
+            );
+          }
+          return true;
       }
     ];
   }
@@ -91,13 +115,41 @@ class RestApiParams
       'type'        => 'string',
       'format'      => 'uri',
       'required'    => true,
+      'sanitize_callback' => function ($token) {
+          return substr($this->wpService->sanitizeTextField($token), 0, 32);
+      },
       'validate_callback' => function ($token, $request) {
           $postId = $request->get_params()['post-id'] ?? null;
           $post   = $this->wpService->getPost($postId);
+
+          // Not a post
           if(is_a($post, 'WP_Post') === false) {
-            return false;
+            return new WP_Error(
+              'invalid_post_id',
+              __('The resource you are trying to find, does not exist.', 'modularity-frontend-form'),
+              ['status' => 404]
+            );
           }
-          return (bool) $token == $post->post_password && $post->post_password !== '';
+
+          // Unprotected post
+          if(empty($post->post_password)) {
+            return new WP_Error(
+              'asset_not_protected',
+              __('The resource you are trying to access is not editable.', 'modularity-frontend-form'),
+              ['status' => 403]
+            );
+          }
+
+          //Not matching the token
+          if($token !== $post->post_password) {
+            return new WP_Error(
+              'invalid_token',
+              __('The token provided, does not match the asset.', 'modularity-frontend-form'),
+              ['status' => 403]
+            );
+          }
+
+          return true;
       }
     ];
   }

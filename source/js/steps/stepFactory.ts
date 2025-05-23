d0c015f68ab274cf5c25def343c39cade9ef4310
NiclasNorin <niclasn@outlook.com>
2025-05-23 10:16:04 +0200

    fix: passing step container

--- source/js/steps/stepFactory.ts
+++ source/js/steps/stepFactory.ts
@@ -2,8 +2,9 @@ import AnimateStepHelper from './animation/animateStepHelper';
 import Step from './step';
 
 class StepFactory {
-    public static createStep(editButton: HTMLElement, step: HTMLElement, id: number): Step {
+    public static createStep(stepContainer: HTMLElement, editButton: HTMLElement, step: HTMLElement, id: number): Step {
         return new Step(
+            stepContainer,
             editButton,
             step,
             new AnimateStepHelper(step),

a14a9bb53a594ab5421c1ae1520c626636296ee9
GitHub Action <>
2025-09-26 12:48:24 +0000

    build: update package file versions to 0.51.2

--- modularity-frontend-form.php
+++ modularity-frontend-form.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Frontend Form
  * Plugin URI:        https://github.com/helsingborg-stad/modularity-frontend-form
  * Description:       A plugin to create a modularity module for a form.
- * Version: 0.51.1
+ * Version: 0.51.2
  * Author:            Niclas Norin, Sebastian Thulin
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT

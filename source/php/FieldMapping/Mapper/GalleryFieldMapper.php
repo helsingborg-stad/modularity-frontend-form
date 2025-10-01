005dcf026c4ebbcb5b5e03fddb90a9d64ca32057
NiclasNorin <niclasn@outlook.com>
2025-10-01 11:40:39 +0200

    fix: default to 50 (like the component)

--- source/php/FieldMapping/Mapper/GalleryFieldMapper.php
+++ source/php/FieldMapping/Mapper/GalleryFieldMapper.php
@@ -19,9 +19,9 @@ class GalleryFieldMapper implements FieldMapperInterface
             $mapped['accept'] = !empty($this->field['mime_types'])
             ? str_replace(' ', ',', $this->field['mime_types'])
             : 'image/*';
-            
-            $mapped['filesMax'] = !empty($this->fields['max']) ? $this->fields['max'] : 100;
-            $mapped['maxSize'] = !empty($this->fields['max_size']) ? $this->fields['max_size'] : 10;
+
+            $mapped['filesMax'] = !empty($this->field['max']) ? $this->field['max'] : 50;
+            $mapped['maxSize'] = !empty($this->field['max_size']) ? $this->field['max_size'] : 10;
         }
 
         return $mapped ?? null;

af1dfc36449671bc6307d7f62477fff557b87bc8
NiclasNorin <niclasn@outlook.com>
2025-10-07 08:23:10 +0200

    refactor: solidify

--- source/js/fields/field/wysiwyg/editor/contentArea.ts
+++ source/js/fields/field/wysiwyg/editor/contentArea.ts
@@ -3,9 +3,9 @@ import Editor from "./editor";
 
 class ContentArea implements ContentAreaInterface {
     private contentArea!: HTMLElement;
-    private formatBlock = 'formatBlock';
-    private defaultParagraphSeparator = 'p';
-    private defaultParagraphSeparatorString = 'defaultParagraphSeparator';
+    private formatBlock = "formatBlock";
+    private defaultParagraphSeparator = "p";
+    private defaultParagraphSeparatorString = "defaultParagraphSeparator";
 
     constructor(
         private config: EditorConfigInterface,
@@ -17,34 +17,65 @@ class ContentArea implements ContentAreaInterface {
     }
 
     public appendContentArea(): HTMLElement {
-        const div = document.createElement('div');
+        const div = document.createElement("div");
         div.className = this.config.getClasses().content;
-        div.contentEditable = 'true';
-
+        div.contentEditable = "true";
         this.config.getElement().appendChild(div);
         this.contentArea = div;
         return div;
     }
 
     public setupChangeListener(): void {
-        this.contentArea.addEventListener('input', ({ target }) => {
-            const firstChild = (target as HTMLElement).firstChild;
-            if (firstChild && firstChild.nodeType === 3) {
-                exec(this.formatBlock, `<${this.defaultParagraphSeparator}>`);
-            } else if (this.contentArea.innerHTML === '<br>') {
-                this.contentArea.innerHTML = '';
-            }
-
-            this.editor.runChangeListeners(this.contentArea.innerHTML);
-        });
-
-        this.contentArea.addEventListener('keydown', (event: KeyboardEvent) => {
-            if (event.key === 'Enter') {
-                setTimeout(() => exec('formatBlock', `<${this.defaultParagraphSeparator}>`), 0);
-            }
-        });
-
+        this.contentArea.addEventListener("input", this.handleInput.bind(this));
+        this.contentArea.addEventListener("keydown", this.handleKeyDown.bind(this));
         exec(this.defaultParagraphSeparatorString, this.defaultParagraphSeparator);
+    }
+
+    /** ----------------------------
+     * Event Handlers
+     * ---------------------------- */
+    private handleInput(event: Event): void {
+        const el = event.target as HTMLElement;
+        const firstChild = el.firstChild;
+        const shouldCleanFont = this.shouldCleanFont(el, firstChild);
+
+        if (shouldCleanFont) {
+            this.cleanFontTags(el);
+        }
+
+        this.ensureParagraphBlock(el, firstChild);
+
+        this.editor.runChangeListeners(el.innerHTML);
+    }
+
+    private handleKeyDown(event: KeyboardEvent): void {
+        if (event.key === "Enter") {
+            setTimeout(() => exec("formatBlock", `<${this.defaultParagraphSeparator}>`), 0);
+        }
+    }
+
+    /** ----------------------------
+     * Utility Methods
+     * ---------------------------- */
+
+    private shouldCleanFont(el: HTMLElement, firstChild: ChildNode | null): boolean {
+        return firstChild !== null || el.innerHTML === "<br>";
+    }
+
+    private cleanFontTags(el: HTMLElement): void {
+        el.innerHTML = el.innerHTML.replace(/<\/?font[^>]*>/gi, "");
+    }
+
+    private ensureParagraphBlock(el: HTMLElement, firstChild: ChildNode | null): void {
+        if (firstChild && firstChild.nodeType === 3) {
+            exec(this.formatBlock, `<${this.defaultParagraphSeparator}>`);
+        } else if (
+            !firstChild ||
+            el.innerHTML === "<br>" ||
+            el.innerHTML === `<${this.defaultParagraphSeparator}><br></${this.defaultParagraphSeparator}>`
+        ) {
+            el.innerHTML = "";
+        }
     }
 }
 

cce97bd83f01cd2752dfa780d59f655eb062839d
NiclasNorin <niclasn@outlook.com>
2025-11-20 14:46:23 +0100

    refactor: adding title to wysiwyg

--- source/php/Module/views/fields/wysiwyg.blade.php
+++ source/php/Module/views/fields/wysiwyg.blade.php
@@ -3,6 +3,16 @@
     'id' => $field['id'],
     'classList' => $field['classList']
 ])
+   @if (!empty($field['label']))
+        @typography([
+            'element' => 'span',
+            'classList' => [
+                'mod-frontend-form__custom-label'
+            ],
+        ])
+            {{ $field['label'] }} {!! !empty($field['required']) ? '<span class="u-color__text--danger">*</span>' : '' !!}
+        @endtypography
+    @endif
     @field([
         'type' => 'hidden',
         'attributeList' => [

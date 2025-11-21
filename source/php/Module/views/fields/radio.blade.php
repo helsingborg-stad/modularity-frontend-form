408a21921f5a53bfe014416476e03d2bca9474c8
NiclasNorin <niclasn@outlook.com>
2025-11-21 10:02:45 +0100

    refactor: adding title to checkbox

--- source/php/Module/views/fields/radio.blade.php
+++ source/php/Module/views/fields/radio.blade.php
@@ -3,6 +3,16 @@
     'classList' => $field['classList'],
     'id'        => $field['id']
 ])
+    @if (!empty($field['label']))
+        @typography([
+            'element' => 'span',
+            'classList' => [
+                'mod-frontend-form__custom-label'
+            ],
+        ])
+            {{ $field['label'] }} {!! !empty($field['attributeList']['data-js-required']) ? '<span class="u-color__text--danger">*</span>' : '' !!}
+        @endtypography
+    @endif
     @foreach ($field['choices'] as $choice)
         @option($choice)
         @endoption

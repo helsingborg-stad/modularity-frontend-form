84a462d76010d1467551ce739553552de0e8d3ba
NiclasNorin <niclasn@outlook.com>
2025-12-10 09:57:46 +0100

    feat: loading select

--- source/js/fields/field/select/load/selectValueLoader.ts
+++ source/js/fields/field/select/load/selectValueLoader.ts
@@ -6,8 +6,33 @@ class SelectValueLoader implements SelectValueLoaderInterface {
 	}
 
 	public load(value: any): void {
-		console.log(value);
-		return;
+		if (!value || value === null || value === undefined) {
+			return;
+		}
+
+		const values = this.convertValueToArrayWithStrings(value);
+		const options = this.parent.getOptions();
+
+		options.forEach((option) => {
+			if (values.includes(option.value)) {
+				option.selected = true;
+			} else {
+				option.selected = false;
+			}
+		});
+		this.parent.getField().dispatchEvent(new Event('change'));
+	}
+
+	private convertValueToArrayWithStrings(value: any): string[] {
+		let values = [];
+
+		if (Array.isArray(value)) {
+			values = value.map((val) => String(val));
+		} else {
+			values = [String(value)];
+		}
+
+		return values;
 	}
 }
 

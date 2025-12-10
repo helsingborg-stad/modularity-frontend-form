d080bc3c1b3f6d9932b4acf201c7af3eae6f47f3
NiclasNorin <niclasn@outlook.com>
2025-12-10 17:02:08 +0100

    fix: col span for not supported field notice

--- source/php/Module/views/fields/error.blade.php
+++ source/php/Module/views/fields/error.blade.php
@@ -1,5 +1,6 @@
 @notice([
     'type' => 'danger',
+    'classList' => ['o-layout-grid--col-span-12'],
     'message' => [
         'text' => $lang->followingFieldIsNotSupported . ': ' . $field['unsupportedFieldType'] . ' (' . $field['label'] . ')'
     ],

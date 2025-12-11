c2de1e5212f79b264cab28699f3aad3a42f84165
NiclasNorin <niclasn@outlook.com>
2025-12-11 14:55:13 +0100

    feat: repeater load value

--- source/js/fields/field/repeater/load/repeaterValueLoader.ts
+++ source/js/fields/field/repeater/load/repeaterValueLoader.ts
@@ -13,19 +13,38 @@ class RepeaterValueLoader implements RepeaterValueLoaderInterface {
 			return;
 		}
 
-		console.log(this.repeaterUI.getRowCount());
-		const rowCount = this.repeaterUI.getRowCount();
-		if (rowCount === 0) {
+		const linesToCreate = value.length - this.repeaterUI.getRowCount();
+
+		for (let i = 0; i < linesToCreate; i++) {
+			this.repeaterUI.buildRow(true, false);
 		}
 
-		value.forEach((row) => {
-			this.repeaterUI.buildRow(true, false);
-		});
+		const rowFieldsObject = this.repeaterUI.getRows();
 
-		console.log(value);
-		console.log(this.repeaterUI.getRows());
-		console.log(this.parent.getName());
-		return;
+		for (const [index, rowValue] of value.entries()) {
+			if (typeof rowValue !== 'object' || rowValue === null) {
+				console.error(`Invalid row value at index ${index}. Expected an object.`);
+				continue;
+			}
+
+			this.addDataToRow(index, rowValue, rowFieldsObject);
+		}
+	}
+
+	private addDataToRow(rowIndex: number, rowValue: { [key: string]: any }, rowFieldsObject: RowFieldsObject): void {
+		for (const [fieldName, fieldValue] of Object.entries(rowValue)) {
+			const keyToLookForInRowFieldsObject = `[${this.parent.getName()}][${rowIndex}][${fieldName}]`;
+
+			if (!rowFieldsObject[rowIndex] || !rowFieldsObject[rowIndex][keyToLookForInRowFieldsObject]) {
+				console.error(
+					`Field with name ${keyToLookForInRowFieldsObject} not found in row ${rowIndex}. Skipping this field.`,
+				);
+				continue;
+			}
+
+			const field = rowFieldsObject[rowIndex][keyToLookForInRowFieldsObject];
+			field.getValueLoader().load(fieldValue);
+		}
 	}
 }
 

ca7713cde4837e826606c7e479b439e2da2ead82
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2025-12-15 17:00:37 +0100

    fix: adds a check outline to check that number of files are within limited amout

---
+++ source/php/DataProcessor/Validators/FilesCountIsWithinLimits.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ModularityFrontendForm\DataProcessor\Validators;
+
+use AcfService\AcfService;
+use ModularityFrontendForm\Config\ConfigInterface;
+use ModularityFrontendForm\Config\ModuleConfigInterface;
+use ModularityFrontendForm\DataProcessor\Validators\Result\ValidationResult;
+use ModularityFrontendForm\DataProcessor\Validators\Result\ValidationResultInterface;
+use WP_Error;
+use WpService\WpService;
+use ModularityFrontendForm\Config\GetModuleConfigInstanceTrait;
+use ModularityFrontendForm\Api\RestApiResponseStatusEnums;
+use WP_REST_Request;
+
+class FilesCountIsWithinLimits implements ValidatorInterface
+{
+    public function __construct(
+        private WpService $wpService,
+        private AcfService $acfService,
+        private ConfigInterface $config,
+        private ModuleConfigInterface $moduleConfigInstance,
+        private ValidationResultInterface $validationResult = new ValidationResult()
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function validate(array $data, WP_REST_Request $request): ?ValidationResultInterface
+    {
+      return $this->validationResult;
+    }
+}

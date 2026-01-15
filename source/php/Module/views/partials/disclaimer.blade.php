a58566323d8ed1abc631d51f54ff0eb026e29828
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-15 11:25:46 +0100

    fix: move disclaimer to own partial

---
+++ source/php/Module/views/partials/disclaimer.blade.php
@@ -0,0 +1,10 @@
+@typography([
+    'variant' => 'meta',
+    'classList' => [
+        'mod-frontend-form__disclaimer',
+        'u-margin__x--4',
+        'u-margin__top--4',
+    ]
+])
+  {!! $disclaimerText !!}
+@endtypography

87164caa14b2223ba1494fb604744527318c691d
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-15 11:25:57 +0100

    fix: unset width

--- source/php/Module/views/partials/disclaimer.blade.php
+++ source/php/Module/views/partials/disclaimer.blade.php
@@ -4,6 +4,9 @@
         'mod-frontend-form__disclaimer',
         'u-margin__x--4',
         'u-margin__top--4',
+    ],
+    'attributeList' => [
+        'style' => 'max-width: unset;'
     ]
 ])
   {!! $disclaimerText !!}

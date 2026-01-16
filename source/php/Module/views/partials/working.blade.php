7d3b7721a3595c92625993c7a458b7e947278740
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-16 10:18:06 +0100

    feat: make working status be visible even when user scrolls

--- source/php/Module/views/partials/working.blade.php
+++ source/php/Module/views/partials/working.blade.php
@@ -7,94 +7,98 @@
         'data-js-frontend-form-working' => 'true'
     ]
 ])
-
-    @icon([
-        'icon' => 'send',
-        'size' => 'xxl',
-        'classList' => [
-            'mod-frontend-form__working-icon'
-        ],
-        'attributeList' => [
-            'data-js-frontend-form-working__icon' => 'true'
-        ]
-    ])
-    @endicon
-
-    @progressBar([
-        'classList' => [
-            'mod-frontend-form__working-progress-bar'
-        ],
-        'attributeList' => [
-            'data-js-frontend-form-working__progress' => 'true'
-        ],
-        'value' => 25,
-    ])
-    @endprogressBar
-
     @element(['classList' => [
-        'u-display--flex',
-        'u-flex--column',
-        'u-align-items--center',
-        'u-justify-content--center',
-        'u-flex-direction--column',
-        'u-gap--1']
-    ])
-        @typography([
-            'element' => 'div',
-            'variant' => 'body',
-            'classList' => [
-                'mod-frontend-form__working-title'
-            ],
-            'attributeList' => [
-                'data-js-frontend-form-working__title' => 'true'
-            ],
-        ])
-            {{ $lang->submitting ?? 'Submitting'}}
-        @endtypography
-        
-        @typography([
-            'element' => 'div',
-            'variant' => 'meta',
-            'classList' => [
-                'mod-frontend-form__working-description'
-            ],
-            'attributeList' => [
-                'data-js-frontend-form-working__description' => 'true'
-            ],
-        ])
-            {{ $lang->submitInit ?? 'Preparing' }}
-        @endtypography
-    @endelement
-    @element([
-        'classList' => [
-            'mod-frontend-form__working-button'
+        'mod-frontend-form__working-content'
         ]
     ])
-        @button([
-            'style' => 'outlined',
-            'color' => 'secondary',
-            'size' => 'md',
+        @icon([
+            'icon' => 'send',
+            'size' => 'xxl',
             'classList' => [
-                'u-display--none'
+                'mod-frontend-form__working-icon'
             ],
             'attributeList' => [
-                'data-js-frontend-form-working-return-button' => 'true'
-            ],
-            'text' => $lang->return ?? 'Return'
+                'data-js-frontend-form-working__icon' => 'true'
+            ]
         ])
-        @endbutton
-        @button([
-            'style' => 'filled',
-            'color' => 'secondary',
-            'size' => 'md',
+        @endicon
+
+        @progressBar([
             'classList' => [
-                'u-display--none'
+                'mod-frontend-form__working-progress-bar'
             ],
             'attributeList' => [
-                'data-js-frontend-form-working-try-again-button' => 'true'
+                'data-js-frontend-form-working__progress' => 'true'
             ],
-            'text' => $lang->tryAgain ?? 'Try again'
+            'value' => 25,
         ])
-        @endbutton
+        @endprogressBar
+
+        @element(['classList' => [
+            'u-display--flex',
+            'u-flex--column',
+            'u-align-items--center',
+            'u-justify-content--center',
+            'u-flex-direction--column',
+            'u-gap--1']
+        ])
+            @typography([
+                'element' => 'div',
+                'variant' => 'body',
+                'classList' => [
+                    'mod-frontend-form__working-title'
+                ],
+                'attributeList' => [
+                    'data-js-frontend-form-working__title' => 'true'
+                ],
+            ])
+                {{ $lang->submitting ?? 'Submitting'}}
+            @endtypography
+            
+            @typography([
+                'element' => 'div',
+                'variant' => 'meta',
+                'classList' => [
+                    'mod-frontend-form__working-description'
+                ],
+                'attributeList' => [
+                    'data-js-frontend-form-working__description' => 'true'
+                ],
+            ])
+                {{ $lang->submitInit ?? 'Preparing' }}
+            @endtypography
+        @endelement
+        @element([
+            'classList' => [
+                'mod-frontend-form__working-button'
+            ]
+        ])
+            @button([
+                'style' => 'outlined',
+                'color' => 'secondary',
+                'size' => 'md',
+                'classList' => [
+                    'u-display--none'
+                ],
+                'attributeList' => [
+                    'data-js-frontend-form-working-return-button' => 'true'
+                ],
+                'text' => $lang->return ?? 'Return'
+            ])
+            @endbutton
+            @button([
+                'style' => 'filled',
+                'color' => 'secondary',
+                'size' => 'md',
+                'classList' => [
+                    'u-display--none'
+                ],
+                'attributeList' => [
+                    'data-js-frontend-form-working-try-again-button' => 'true'
+                ],
+                'text' => $lang->tryAgain ?? 'Try again'
+            ])
+            @endbutton
+        @endelement
     @endelement
 @endelement

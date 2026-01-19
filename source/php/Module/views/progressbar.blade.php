c16d2e9a12e237458f63d610ce83abcab924634e
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2026-01-19 12:24:39 +0100

    fix: improve mobile ui

--- source/php/Module/views/progressbar.blade.php
+++ source/php/Module/views/progressbar.blade.php
@@ -1,132 +1,138 @@
 @element([
-    'componentElement' => 'ul',
     'classList' => [
-        'mod-frontend-form__progressbar',
-    ],
-    'attributeList' => [
-        'data-js-frontend-form-progressbar' => 'true',
+        'mod-frontend-form__progressbar-wrapper'
     ]
 ])
-    @foreach($steps as $index => $step)
-        @element([
-            'componentElement' => 'li',
-            'classList' => [
-                'mod-frontend-form__progressbar-step',
-                'u-margin__top--0',
-                $loop->first ? 'is-active' : '',
-                $loop->first ? 'is-editable' : '',
-                $loop->first ? 'is-first' : '',
-                $loop->last  ? 'is-last' : '',
-            ],
-            'attributeList' => [
-                'data-js-frontend-form-progressbar-step' => $index
-            ]
-        ])
-
-            {{-- Step icon & button --}}
-            @element([
-                'componentElement' => 'button',
-                'classList' => [
-                    'mod-frontend-form__progressbar-step-button',
-                ],
-                'attributeList' => [
-                    'type' => 'button',
-                    'aria-label' => 'Go to step ' . ($index + 1) . ': ' . $step['title'],
-                ]
-            ])
-                @icon([
-                    'classList' => [
-                        'mod-frontend-form__progressbar-step-button-icon',
-                    ],
-                    'icon' => $step['icon'] ?: 'counter_' . ($index + 1),
-                    'size' => 'md'
-                ])
-                @endicon
-            @endelement
-
-            {{-- Step title & description --}}
-            @element([
-                'classList' => [
-                    'mod-frontend-form__progressbar-step-content',
-                ]
-            ])
-                @typography([
-                    'element' => 'h4',
-                    'variant' => 'h4',
-                    'classList' => [
-                        'mod-frontend-form__progressbar-step-title',
-                        'u-color__text--darker',
-                        'u-bold',
-                        'u-text-small'
-                    ]
-                ])
-                    {{ $step['title'] }}
-                @endtypography
-
-                @typography([
-                    'element' => 'div',
-                    'variant' => 'meta',
-                    'classList' => [
-                        'mod-frontend-form__progressbar-step-number',
-                    ]
-                ])
-                    Step {{ $index + 1 }}/{{ count($steps) }}
-                @endtypography
-            @endelement
-
-            {{-- Status icon: OK --}}
-            @icon([
-                'classList' => [
-                    'mod-frontend-form__progressbar-step-status-icon',
-                    'mod-frontend-form__progressbar-step-status-icon-ok',
-                ],
-                'icon' => 'check',
-                'size' => 'md',
-                'attributeList' => [
-                    'aria-label' => 'Completed',
-                    'aria-hidden' => 'true'
-                ]
-            ])
-            @endicon
-
-            {{-- Status icon: ERR --}}
-            @icon([
-                'classList' => [
-                    'mod-frontend-form__progressbar-step-status-icon',
-                    'mod-frontend-form__progressbar-step-status-icon-error',
-                ],
-                'icon' => 'error',
-                'size' => 'md',
-                'attributeList' => [
-                    'aria-label' => 'Error',
-                    'aria-hidden' => 'true'
-                ]
-            ])
-            @endicon
-
-        @endelement
-
-        {{-- Divider --}}
-        @if(!$loop->last)
+    @element([
+        'componentElement' => 'ul',
+        'classList' => [
+            'mod-frontend-form__progressbar',
+        ],
+        'attributeList' => [
+            'data-js-frontend-form-progressbar' => 'true',
+        ]
+    ])
+        @foreach($steps as $index => $step)
             @element([
                 'componentElement' => 'li',
                 'classList' => [
-                    'mod-frontend-form__progressbar-divider',
+                    'mod-frontend-form__progressbar-step',
+                    'u-margin__top--0',
+                    $loop->first ? 'is-active' : '',
+                    $loop->first ? 'is-editable' : '',
+                    $loop->first ? 'is-first' : '',
+                    $loop->last  ? 'is-last' : '',
                 ],
                 'attributeList' => [
-                    'aria-hidden' => 'true'
+                    'data-js-frontend-form-progressbar-step' => $index
                 ]
             ])
-                @icon([
-                    'icon' => 'chevron_right',
-                    'size' => 'md',
+
+                {{-- Step icon & button --}}
+                @element([
+                    'componentElement' => 'button',
                     'classList' => [
-                        'mod-frontend-form__progressbar-divider-icon',
+                        'mod-frontend-form__progressbar-step-button',
+                    ],
+                    'attributeList' => [
+                        'type' => 'button',
+                        'aria-label' => 'Go to step ' . ($index + 1) . ': ' . $step['title'],
+                    ]
+                ])
+                    @icon([
+                        'classList' => [
+                            'mod-frontend-form__progressbar-step-button-icon',
+                        ],
+                        'icon' => $step['icon'] ?: 'counter_' . ($index + 1),
+                        'size' => 'md'
+                    ])
+                    @endicon
+                @endelement
+
+                {{-- Step title & description --}}
+                @element([
+                    'classList' => [
+                        'mod-frontend-form__progressbar-step-content',
+                    ]
+                ])
+                    @typography([
+                        'element' => 'h4',
+                        'variant' => 'h4',
+                        'classList' => [
+                            'mod-frontend-form__progressbar-step-title',
+                            'u-color__text--darker',
+                            'u-bold',
+                            'u-text-small'
+                        ]
+                    ])
+                        {{ $step['title'] }}
+                    @endtypography
+
+                    @typography([
+                        'element' => 'div',
+                        'variant' => 'meta',
+                        'classList' => [
+                            'mod-frontend-form__progressbar-step-number',
+                        ]
+                    ])
+                        Step {{ $index + 1 }}/{{ count($steps) }}
+                    @endtypography
+                @endelement
+
+                {{-- Status icon: OK --}}
+                @icon([
+                    'classList' => [
+                        'mod-frontend-form__progressbar-step-status-icon',
+                        'mod-frontend-form__progressbar-step-status-icon-ok',
+                    ],
+                    'icon' => 'check',
+                    'size' => 'md',
+                    'attributeList' => [
+                        'aria-label' => 'Completed',
+                        'aria-hidden' => 'true'
                     ]
                 ])
                 @endicon
-            @endelement
-        @endif
 
-    @endforeach
+                {{-- Status icon: ERR --}}
+                @icon([
+                    'classList' => [
+                        'mod-frontend-form__progressbar-step-status-icon',
+                        'mod-frontend-form__progressbar-step-status-icon-error',
+                    ],
+                    'icon' => 'error',
+                    'size' => 'md',
+                    'attributeList' => [
+                        'aria-label' => 'Error',
+                        'aria-hidden' => 'true'
+                    ]
+                ])
+                @endicon
+
+            @endelement
+
+            {{-- Divider --}}
+            @if(!$loop->last)
+                @element([
+                    'componentElement' => 'li',
+                    'classList' => [
+                        'mod-frontend-form__progressbar-divider',
+                    ],
+                    'attributeList' => [
+                        'aria-hidden' => 'true'
+                    ]
+                ])
+                    @icon([
+                        'icon' => 'chevron_right',
+                        'size' => 'md',
+                        'classList' => [
+                            'mod-frontend-form__progressbar-divider-icon',
+                        ]
+                    ])
+                    @endicon
+                @endelement
+            @endif
+
+        @endforeach
+    @endelement
 @endelement

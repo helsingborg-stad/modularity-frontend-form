d30c1842f9c809f4708741daf2fb323623edf987
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2026-06-04 15:42:31 +0200

    fix: boolean payload values being sent as string

--- source/php/DataProcessor/Handlers/WebHookHandler.php
+++ source/php/DataProcessor/Handlers/WebHookHandler.php
@@ -2,7 +2,7 @@
 
 namespace ModularityFrontendForm\DataProcessor\Handlers;
 
-use WpService\WpService; 
+use WpService\WpService;
 use AcfService\AcfService;
 use ModularityFrontendForm\Config\GetModuleConfigInstanceTrait;
 use ModularityFrontendForm\Config\ConfigInterface;
@@ -16,20 +16,21 @@ use ModularityFrontendForm\Hydratable\JsonDotHydrator;
 use WP_Error;
 use WP_REST_Request;
 
-class WebHookHandler implements HandlerInterface {
+class WebHookHandler implements HandlerInterface
+{
 
   use GetModuleConfigInstanceTrait;
 
   public function __construct(
-      private WpService $wpService,
-      private AcfService $acfService,
-      private ConfigInterface $config,
-      private ModuleConfigInterface $moduleConfigInstance,
-      private object $params,
-      private HandlerResultInterface $handlerResult = new HandlerResult(),
-      private ?FileHandlerInterface $fileHandler = null
+    private WpService $wpService,
+    private AcfService $acfService,
+    private ConfigInterface $config,
+    private ModuleConfigInterface $moduleConfigInstance,
+    private object $params,
+    private HandlerResultInterface $handlerResult = new HandlerResult(),
+    private ?FileHandlerInterface $fileHandler = null
   ) {
-    if($this->fileHandler === null) {
+    if ($this->fileHandler === null) {
       $this->fileHandler = new NullFileHandler($this->config, $this->moduleConfigInstance, $this->wpService);
     }
   }
@@ -44,11 +45,15 @@ class WebHookHandler implements HandlerInterface {
   {
     $config = $this->moduleConfigInstance->getWebHookHandlerConfig();
 
-    if($this->validateCallbackUrl($config->callbackUrl) === false) {
+    if ($this->validateCallbackUrl($config->callbackUrl) === false) {
       return $this->handlerResult;
     }
 
-    if($this->trySendRequest($config->callbackUrl, $this->createBody($data, $config), $this->createHeaders($data, $config))) {
+    if ($this->trySendRequest(
+      $config->callbackUrl,
+      $this->createBody($data, $config),
+      $this->createHeaders($data, $config)
+    )) {
       return $this->handlerResult;
     }
 
@@ -67,7 +72,7 @@ class WebHookHandler implements HandlerInterface {
     if (!filter_var($url, FILTER_VALIDATE_URL)) {
       $this->handlerResult->setError(
         new WP_Error(
-          RestApiResponseStatusEnums::HandlerError->value, 
+          RestApiResponseStatusEnums::HandlerError->value,
           $this->wpService->__('Invalid callback url format.', 'modularity-frontend-form')
         )
       );
@@ -79,7 +84,7 @@ class WebHookHandler implements HandlerInterface {
     if ($headers === false) {
       $this->handlerResult->setError(
         new WP_Error(
-          RestApiResponseStatusEnums::HandlerError->value, 
+          RestApiResponseStatusEnums::HandlerError->value,
           $this->wpService->__('Callback url is not reachable.', 'modularity-frontend-form')
         )
       );
@@ -98,17 +103,16 @@ class WebHookHandler implements HandlerInterface {
    */
   private function trySendRequest(string $url, array $data, array $headers = []): bool
   {
-    error_log(print_r($headers, true));
     $response = $this->wpService->wpRemotePost($url, [
       'body' => \json_encode($data),
       'timeout' => 20,
       'headers' => $headers
     ]);
 
-    if($this->wpService->isWpError($response)) {
+    if ($this->wpService->isWpError($response)) {
       $this->handlerResult->setError(
         new WP_Error(
-          RestApiResponseStatusEnums::HandlerError->value, 
+          RestApiResponseStatusEnums::HandlerError->value,
           $this->wpService->__('Failed to send data to webhook.', 'modularity-frontend-form')
         )
       );
@@ -118,38 +122,56 @@ class WebHookHandler implements HandlerInterface {
     return true;
   }
 
-    private function createBody(array $data, object $config): array {
+  private function createBody(array $data, object $config): array
+  {
     $formData = $this->parseFormData($data['mod-frontend-form']);
-    return empty($config->body) 
-      ? $formData 
+
+    return empty($config->body)
+      ? $formData
       : \json_decode(
-        (new JsonDotHydrator())->hydrate($config->body, $formData), 
+        (new JsonDotHydrator())->hydrate($config->body, $formData),
         true
       );
   }
 
-  private function parseFormData(array $formData): array {
-    $dataAsJson = \json_encode($formData);
-    preg_match_all('/field_[a-zA-Z0-9_]+/', $dataAsJson, $matches);
-    $replaceIdWithNames = array_map(
-      fn($v) => $this->acfService->getFieldObject($v)['name'], 
-      array_combine($matches[0], $matches[0])
-    );
-    
-    $dataAfterReplace = \json_decode(
-      strtr($dataAsJson, $replaceIdWithNames), 
-      true
-    );
-
-    return $dataAfterReplace;
-  }
-  
-  private function createHeaders(array $data, object $config): array {
+  private function createHeaders(array $data, object $config): array
+  {
     return array_merge(
       [
         'Content-Type' => 'application/json',
       ],
       array_column($config->headers ?? [], 'value', 'header')
     );
+  }
+
+  private function parseFormData(array $formData): array
+  {
+    $unserializersByFieldType = [
+      'true_false'  => fn($v) => (bool)(int) $v,
+      'null'        => fn($v) => $v
+    ];
+
+    $fieldObjects = array_map(
+      fn($fieldId) => $this->acfService->getFieldObject($fieldId),
+      array_combine(array_keys($formData), array_keys($formData))
+    );
+
+    foreach ($fieldObjects as $fieldId => $fieldObject) {
+      $unserializer = in_array($fieldObject['type'], array_keys($unserializersByFieldType))
+        ? $fieldObject['type']
+        : 'null';
+      $formData[$fieldId] = $unserializersByFieldType[$unserializer]($formData[$fieldId]);
+    }
+
+    $dataAsJson = \json_encode($formData);
+
+    preg_match_all('/field_[a-zA-Z0-9_]+/', $dataAsJson, $matches);
+
+    $replaceIdWithNames = array_map(
+      fn($v) => $fieldObjects[$v]['name'] ?? $v,
+      array_combine($matches[0], $matches[0])
+    );
+
+    return \json_decode(strtr($dataAsJson, $replaceIdWithNames), true);
   }
 }

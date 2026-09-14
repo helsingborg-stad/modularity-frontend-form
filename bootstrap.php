68cca2f4523e4916667592e07735eacb59408269
Nikolas Ramstedt <nikolas.ramstedt@helsingborg.se>
2026-09-15 00:11:36 +0200

    test: define WordPress result-format constants

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,11 @@
 <?php
 
+// WordPress result-format constants are needed when PHPUnit mocks WpService.
+foreach (['OBJECT', 'OBJECT_K', 'ARRAY_A', 'ARRAY_N'] as $format) {
+    if (!defined($format)) {
+        define($format, $format);
+    }
+}
+
 include_once __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
 require_once __DIR__ . '/vendor/autoload.php';
